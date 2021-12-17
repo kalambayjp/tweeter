e99858e44fdf7e7f308f6e3187d85038489be128
JP <kalambayjp@gmail.com>
2021-12-16 17:52:52 -0800

    final cut

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,64 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = (tweetObj) => {
+  // create new tweet aricle
+  let $newTweet = $(`<article class="tweet"></article>`);
+
+  // new tweet header
+  $(`<header><p><img src=${tweetObj.user.avatars}>&nbsp${tweetObj.user.name}</p><p>${tweetObj.user.handle}</p></header>`).appendTo($newTweet);
+
+  // new tweet text
+  $(`<p class="tweet-text">`).text(tweetObj.content.text).appendTo($newTweet);
+
+  // new tweet footer
+  $(`<footer><p>${timeago.format(tweetObj.created_at)}</p><p class="icons"><i class="fa-solid fa-flag"></i>&nbsp<i class="fa-solid fa-retweet">&nbsp</i><i class="fa-solid fa-heart"></i></p>`).appendTo($newTweet);
+
+  return $newTweet;
+};
+
+const renderTweets = (tweets) => {
+  tweets.forEach(element => {
+    let $newTweet = createTweetElement(element);
+    $(".tweets-container").prepend($newTweet);
+  })
+};
+
+const loadTweets = () => {
+  $.get("tweets", function(data, status) {
+
+  renderTweets(data);
+  })
+};
+
+$(document).ready(() =>  {
+
+  loadTweets();
+
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    let tweetLength = $(`#tweet-text`).val().length;
+
+    if (tweetLength === 0) {
+      $(".empty-str").slideDown(400); // make "no value" error message visible
+    } else if (tweetLength > 140) {
+      $(".too-long").slideDown(400);  // make "too long" error message visible
+    } else {
+      $.post("tweets", $(this).serialize(), function(data, status) {
+        // remove any potential error messages
+        $(".empty-str").hide();
+        $(".too-long").hide();
+
+        // reset character count
+        $(".counter").val('140');
+        
+        loadTweets();
+
+        // clear text feild
+        $(`#tweet-text`).val('');
+      })
+    }
+  });
+  
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,47 +2,18 @@ $(document).ready(() => {
   const $newTweet = $("#tweet-text");
 
   $newTweet.on('keyup', function() {
-    let $charCount = $(this).closest('form').find('output').val();
-    let $remainingCount = 140 - $(this).val().length;
+    let $charCount = $(this).closest('form').find('output').val();         // traversal to find value of the charctercount
+    let $remainingCount = 140 - $(this).val().length;                      
     
-    $charCount = $(this).closest('form').find('output').text($remainingCount);
+    $charCount = $(this).closest('form').find('output').text($remainingCount);// reassign value of character count in the html
     
-
+  // Red error text conditionals
     if ($charCount.val() < 0) {
-      $charCount.attr("id", "exceeds-count");
+      $charCount.css('color', 'red');
     }
 
     if ($charCount.val() >= 0) {
-      $charCount.removeAttr('id');
+      $charCount.css('color', 'revert');
     }
   })
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+});
